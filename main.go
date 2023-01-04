7fc21425f8edd6d99cc61f079fee0ab6d6eeeb48
hikobend <hiko.bkend@gmail.com>
2023-01-04 20:40:16 +0900

    90.0%

--- main.go
+++ main.go
@@ -1,25 +1,34 @@
 package main
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-func HomeHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "hello world",
-	})
+type User struct {
+	Name string `json:"name" binding:"required"`
 }
 
-func SetupServer() *gin.Engine {
+func router() *gin.Engine {
 	r := gin.Default()
-	r.GET("/", HomeHandler)
+
+	r.GET("/ping", func(c *gin.Context) {
+		// const StatusOK untyped int = 200
+		// gin.Hはmap[string]interface{}と同じ
+		c.JSON(http.StatusOK, gin.H{"msg": "pong"})
+	})
+
+	r.POST("/ps", func(c *gin.Context) {
+		var u User
+		if err := c.BindJSON(&u); err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"msg": "error"})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"msg": u})
+	})
 	return r
 }
-
 func main() {
-	log.Println("Server Start...")
-	SetupServer().Run()
+	router().Run()
 }
--- main_test.go
+++ main_test.go
@@ -1,30 +1,62 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/go-playground/assert"
 )
 
-func TestHomeHandler(t *testing.T) {
-	mockUserResp := `{"message":"hello world"}`
-	// テスト用のサーバーを立てる
-	ts := httptest.NewServer(SetupServer())
+func TestPingRouter(t *testing.T) {
+	router := router()
+	w := httptest.NewRecorder()
+	//c, _ := gin.CreateTestContext(w)
+	req, _ := http.NewRequest("GET", "/ping", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	// ...
+	assert.Equal(t, w.Body.String(), "{\"msg\":\"pong\"}")
+}
+
+func TestRouter(t *testing.T) {
+	// create a test server
+	ts := httptest.NewServer(router())
 	defer ts.Close()
-	// リクエストを送れるか?
-	resp, err := http.Get(fmt.Sprintf("%s/", ts.URL))
+
+	// test the POST /ps route
+	res, err := http.Post(ts.URL+"/ps", "application/json", bytes.NewBuffer([]byte(`{"name":"test"}`)))
 	if err != nil {
-		t.Fatalf("Expected no error, got %v", err)
+		t.Errorf("Error making POST request: %v", err)
 	}
-	defer resp.Body.Close()
-	// Statusコードは200か?
-	if resp.StatusCode != http.StatusOK {
-		t.Fatalf("Expected status code 200, got %v", resp.StatusCode)
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("Expected status OK; got %v", res.StatusCode)
 	}
-	responseData, _ := ioutil.ReadAll(resp.Body)
-	if string(responseData) != mockUserResp {
-		t.Fatalf("Expected hello world message, got %v", responseData)
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("Error reading response body: %v", err)
+	}
+	expected := `{"msg":{"name":"test"}}`
+	if string(body) != expected {
+		t.Errorf("Expected response body %q; got %q", expected, string(body))
+	}
+
+	// test the POST /ps route with a request that should fail validation
+	res, err = http.Post(ts.URL+"/ps", "application/json", bytes.NewBuffer([]byte(`{}`)))
+	if err != nil {
+		t.Errorf("Error making POST request: %v", err)
+	}
+	defer res.Body.Close()
+	body, err = ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("Error reading response body: %v", err)
+	}
+	expected = `{"msg":"error"}`
+	if string(body) != expected {
+		t.Errorf("Expected response body %q; got %q", expected, string(body))
 	}
 }
